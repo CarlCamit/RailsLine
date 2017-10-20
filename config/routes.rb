87848e8a68cbac7a414a88af884523f16b15cf18
Carl Camit <carlcamit0@gmail.com>
2017-10-20 15:28:39 +1100

    looking for changed files

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :advisers
   devise_for :users
-  
-  devise_scope :user do
-    authenticated :user do
-      root 'posts#index', as: :authenticated_root
-    end
-    unauthenticated do
-      root 'devise/sessions#new', as: :unauthenticated_root
-    end
-  end
-
   resources :categorizations
   resources :categories
   resources :posts do
     resources :replies
   end
 
-  #root "posts#index"
+  root "posts#index"
   
 end

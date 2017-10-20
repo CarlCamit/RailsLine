f098ad16c5c9c62656064dbf184fc7031080080b
Carl Camit <carlcamit0@gmail.com>
2017-10-20 14:11:21 +1100

    test cleaning of unnecessary files

--- app/uploaders/avatar_image_uploaders.rb
+++ app/uploaders/avatar_image_uploaders.rb
@@ -16,9 +16,9 @@ class AvatarImageUploader < Shrine
   
     def process(io, context)
       if context[:phase] == :store
-        size_700 = resize_to_limit!(io.download, 1440, 300)
-        size_500 = resize_to_limit(size_700,    1440, 300)
-        size_300 = resize_to_limit(size_500,    1400, 300)
+        size_700 = resize_to_limit!(io.download, 700, 700)
+        size_500 = resize_to_limit(size_700, 500, 500)
+        size_300 = resize_to_limit(size_500, 300, 300)
         thumb = resize_to_limit(size_300, 200, 200)
   
         { original: io, large: size_700, medium: size_500, small: size_300, thumb: thumb }
--- app/views/replies/_reply.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! reply, :id, :body, :created_at, :updated_at
-json.url reply_url(reply, format: :json)
